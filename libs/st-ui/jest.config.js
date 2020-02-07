e31b0e02467676303ed4943d3ec1f4bcafc66e1e
geeksterio <atoma@geekster.io>
2020-02-08 00:43:55 +0200

    Renamed ui to st-ui

--- libs/st-ui/jest.config.js
+++ libs/st-ui/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'ui',
+  name: 'st-ui',
   preset: '../../jest.config.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/ui'
+  coverageDirectory: '../../coverage/libs/st-ui'
 };
