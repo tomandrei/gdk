c144094f018d0f303ed56790ec20baa64c845b14
geeksterio <atoma@geekster.io>
2020-02-08 02:56:39 +0200

    Added serve and build commands for stencil - TODO: Fix watch mode and error msg

---
+++ tools/scripts/build-st-ui.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { join } from 'path';
+import { exec } from 'child_process';
+
+const rootDir = process.env.INIT_CWD || process.env.PWD;
+const stencilDir = join(rootDir, 'libs', 'st-ui');
+
+(() => {
+  //TODO: Fix the error display
+  process.chdir(stencilDir);
+  const cmd = 'stencil build --docs';
+
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+  
+    console.log(stdout);
+    console.log(stderr);
+  });
+})();
