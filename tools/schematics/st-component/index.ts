861e6a6fed5af4f5af3ab20a270f00d3fee094c2
geeksterio <atoma@geekster.io>
2020-03-25 18:35:39 +0000

    Small fail net

--- tools/schematics/st-component/index.ts
+++ tools/schematics/st-component/index.ts
@@ -31,25 +31,29 @@ export default function(schema: SchematicOptions): Rule {
         // Add component to the angular module
         const modulePath = 'libs/ng-ui/src/lib/ng-ui.module.ts';
         const moduleFile = tree.read(modulePath);
-        const moduleContent = moduleFile.toString();
+        if(moduleFile) {
+          const moduleContent = moduleFile.toString();
 
-        const regexImports = /(?<=\{\s).+?(?=\s\}* from '\.\/ng-ui';)/g;
-        const from = moduleContent.match(regexImports)[0];
-        const to = `${from}, Gdk${strings.classify(schema.name)}`;
-  
-        tree.overwrite(modulePath, moduleContent.replace(new RegExp(from, 'gi'), to));
+          const regexImports = /(?<=\{\s).+?(?=\s\}* from '\.\/ng-ui';)/g;
+          const from = moduleContent.match(regexImports)[0];
+          const to = `${from}, Gdk${strings.classify(schema.name)}`;
+    
+          tree.overwrite(modulePath, moduleContent.replace(new RegExp(from, 'gi'), to));
+        }
       },
       () => {
         // Add component to the index.html
         const modulePath = 'libs/st-ui/src/index.html';
         const moduleFile = tree.read(modulePath);
-        const moduleContent = moduleFile.toString();
+        if (moduleFile) {
+          const moduleContent = moduleFile.toString();
   
-        const regex = /<link[^>]*>/g;
-        const from = moduleContent.match(regex)[0];
-        const to = `${from}\n  <link rel="import" href="build/gdk-${strings.dasherize(schema.name)}.html" id="gdk-${strings.dasherize(schema.name)}" />`
-  
-        tree.overwrite(modulePath, moduleContent.replace(from, to));
+          const regex = /<link[^>]*>/g;
+          const from = moduleContent.match(regex)[0];
+          const to = `${from}\n  <link rel="import" href="build/gdk-${strings.dasherize(schema.name)}.html" id="gdk-${strings.dasherize(schema.name)}" />`
+    
+          tree.overwrite(modulePath, moduleContent.replace(from, to));
+        }
       }
     ])(tree, context)
   }
